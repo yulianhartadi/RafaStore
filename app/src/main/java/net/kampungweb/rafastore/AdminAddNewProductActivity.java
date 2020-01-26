32b22d7efca43975a5e3ce394a0bec8408b00bbb
yulianhartadi <rawzadigital@gmail.com>
2020-01-26 18:04:44 +0700

    form upload new product, done, belum di test, di firebase console, di menu storage belum di edit untuk akses, jangan lupa di setting dulu

--- app/src/main/java/net/kampungweb/rafastore/AdminAddNewProductActivity.java
+++ app/src/main/java/net/kampungweb/rafastore/AdminAddNewProductActivity.java
@@ -1,5 +1,6 @@
 package net.kampungweb.rafastore;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -9,16 +10,30 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.Toolbar;
 
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import net.kampungweb.rafastore.utils.Tools;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class AdminAddNewProductActivity extends AppCompatActivity {
@@ -31,7 +46,10 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
     private static final int GalleryPick = 1;
     private Uri imageUri;
-
+    private String productRandomKey, downloadImageUrl;
+    private StorageReference productImageRef;
+    private DatabaseReference productsRef;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +57,10 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_admin_add_new_product);
 
         categoryName = getIntent().getExtras().get("Category").toString();
-        Toast.makeText(getApplicationContext(), categoryName, Toast.LENGTH_SHORT).show();
+
+        // connect to storage Firebase
+        productImageRef = FirebaseStorage.getInstance().getReference().child("Product Images");
+        productsRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
         initToolbar();
         initContent();
@@ -53,6 +74,8 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         inputStockProduct = findViewById(R.id.et_stock_product);
         btnCancel = findViewById(R.id.acb_btn_cancel);
         btnAddProduct = findViewById(R.id.acb_btn_add_product);
+
+        progressDialog = new ProgressDialog(this);
 
 
         // add new product
@@ -126,21 +149,21 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
 
             Toast.makeText(this, "Nama produk belum diisi", Toast.LENGTH_SHORT).show();
 
-        } else if (TextUtils.isEmpty(priceProduct)){
+        } else if (TextUtils.isEmpty(priceProduct)) {
 
             Toast.makeText(this, "Deskripsi produk belum diisi", Toast.LENGTH_SHORT).show();
 
-        } else if (TextUtils.isEmpty(descriptionProduct)){
+        } else if (TextUtils.isEmpty(descriptionProduct)) {
 
             Toast.makeText(this, "Deskripsi produk belum diisi", Toast.LENGTH_SHORT).show();
 
-        } else if (TextUtils.isEmpty(stockProduct)){
+        } else if (TextUtils.isEmpty(stockProduct)) {
 
             Toast.makeText(this, "Stock produk belum diisi", Toast.LENGTH_SHORT).show();
 
         } else {
 
-            storeImageInformation();
+            storeProductInformation();
 
         }
 
@@ -151,7 +174,13 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
     }
 
     // Image and product information
-    private void storeImageInformation() {
+    private void storeProductInformation() {
+
+        //progress dialog add new product
+        progressDialog.setTitle("Tambah produk baru");
+        progressDialog.setMessage("Admin di tunggu ya..., Sedang menambahkan produk");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
 
         Calendar calendar = Calendar.getInstance();
 
@@ -161,6 +190,90 @@ public class AdminAddNewProductActivity extends AppCompatActivity {
         SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
         saveCurrentTime = currentTime.format(calendar.getTime());
 
+        productRandomKey = saveCurrentDate + saveCurrentTime;
 
+        final StorageReference filePath = productImageRef.child(imageUri.getLastPathSegment() + productRandomKey + ".jpg");
+
+        final UploadTask uploadTask = filePath.putFile(imageUri);
+
+        // Upload product process
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                String message = e.toString();
+                Toast.makeText(AdminAddNewProductActivity.this, "Error : " + message, Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Toast.makeText(AdminAddNewProductActivity.this, "Produk sukses di upload", Toast.LENGTH_SHORT).show();
+
+                Task<Uri> uriTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+
+                        if (!task.isSuccessful()) {
+                            throw task.getException();
+                        }
+
+                        downloadImageUrl = filePath.getDownloadUrl().toString();
+                        return filePath.getDownloadUrl();
+
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(AdminAddNewProductActivity.this, "Produk sukses tersimpan di database", Toast.LENGTH_SHORT).show();
+                            saveProductInfoToDatabase();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    // upload produk ke Firebase
+    private void saveProductInfoToDatabase() {
+
+        HashMap<String, Object> productMap = new HashMap<>();
+        productMap.put("pid", productRandomKey);
+        productMap.put("date", saveCurrentDate);
+        productMap.put("time", saveCurrentTime);
+        productMap.put("category", categoryName);
+        productMap.put("image", downloadImageUrl);
+        productMap.put("product name", nameProduct);
+        productMap.put("product price", priceProduct);
+        productMap.put("product description", descriptionProduct);
+        productMap.put("product stock", stockProduct);
+
+        productsRef.child(productRandomKey).updateChildren(productMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        if (task.isSuccessful()) {
+
+                            progressDialog.dismiss();
+                            Toast.makeText(AdminAddNewProductActivity.this, "Produk sukses ditambahkan..", Toast.LENGTH_SHORT).show();
+                            Intent categoryIntent = new Intent(AdminAddNewProductActivity.this, AdminCategoryActivity.class);
+                            startActivity(categoryIntent);
+
+                        } else {
+
+                            progressDialog.dismiss();
+                            String message = task.getException().toString();
+                            Toast.makeText(AdminAddNewProductActivity.this, "Produk error : " + message, Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
     }
 }
