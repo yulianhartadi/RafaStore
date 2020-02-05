8352fbb6b1635fd462b1b6dd736a32601f10b46f
yulianhartadi <rawzadigital@gmail.com>
2020-02-05 14:38:08 +0700

    disable dashboard login system for editing another feature

--- app/src/main/java/net/kampungweb/rafastore/UserFragment.java
+++ app/src/main/java/net/kampungweb/rafastore/UserFragment.java
@@ -42,16 +42,16 @@ public class UserFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_user, container, false);
-        Paper.init(Objects.requireNonNull(getContext()));
+        //Paper.init(Objects.requireNonNull(getContext()));
 
         //user image profile pict
         userImagePict = view.findViewById(R.id.civ_user_pict_profile);
 
         //user name
         userFullName = view.findViewById(R.id.tv_user_fullname);
-        if (userFullName != null){
+        /*if (userFullName != null){
             userFullName.setText(Prevalent.currentOnlineUsers.getFullName());
-        }
+        }*/
 
 
         //userlocation
@@ -64,17 +64,17 @@ public class UserFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getActivity(), "Log out", Toast.LENGTH_SHORT).show();
-                Paper.book().destroy();
+                //Paper.book().destroy();
 
 
                 //alternative jump activity
-                Intent logoutIntent = new Intent(getContext(), LoginActivity.class);
+                /*Intent logoutIntent = new Intent(getContext(), LoginActivity.class);
                 logoutIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(logoutIntent);
+                startActivity(logoutIntent);*/
 
                 //close app
-                Objects.requireNonNull(getActivity()).moveTaskToBack(true);
-                getActivity().finish();
+                /*Objects.requireNonNull(getActivity()).moveTaskToBack(true);
+                getActivity().finish();*/
 
             }
         });
